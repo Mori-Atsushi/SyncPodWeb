66922e5c8fb9f75bff86fc11405aa202881a2a39
Mori Atsushi <e1237@s.akashi.ac.jp>
2018-01-10 17:19:18 +0900

    [change] アプリを開くボタンを追加

---
+++ js/room.js
@@ -0,0 +1,7 @@
+var arg = new Object;
+var pair=location.search.substring(1).split('&');
+for(var i=0;pair[i];i++) {
+    var kv = pair[i].split('=');
+    arg[kv[0]]=kv[1];
+}
+document.getElementById("app-room-url").href = "sync-pod://app.com/room?room_key=" + arg.room_key;
