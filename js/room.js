4e22f6ccba7e714f6245c26d34bb53b836e99fa3
Mori Atsushi <e1237@s.akashi.ac.jp>
2018-01-11 20:48:03 +0900

    [change] デザイン修正

--- js/room.js
+++ js/room.js
@@ -5,3 +5,4 @@ for(var i=0;pair[i];i++) {
     arg[kv[0]]=kv[1];
 }
 document.getElementById("app-room-url").href = "sync-pod://app.com/room?room_key=" + arg.room_key;
+document.getElementById("room-key").innerHTML = "ルームキー：" + arg.room_key;
